21079aa81569d006dda843bb4dfc6c3f94ddf35c
Rayane Bengaoui <rayane.bengaoui@gmail.com>
2021-04-08 17:55:30 +0200

    Moving image imports in a data file

--- src/components/HomePage/ViewProjects.js
+++ src/components/HomePage/ViewProjects.js
@@ -3,37 +3,13 @@ import { motion } from "framer-motion";
 
 import ViewProject from "../ViewProject";
 
-import web_design_image_mobile from "../../assets/home/mobile/image-web-design.jpg";
-import web_design_image_tablet from "../../assets/home/tablet/image-web-design.jpg";
-import web_design_image_desktop from "../../assets/home/desktop/image-web-design-large.jpg";
-
-import app_design_image_mobile from "../../assets/home/mobile/image-app-design.jpg";
-import app_design_image_tablet from "../../assets/home/tablet/image-app-design.jpg";
-import app_design_image_desktop from "../../assets/home/desktop/image-app-design.jpg";
-
-import graphic_design_image_mobile from "../../assets/home/mobile/image-graphic-design.jpg";
-import graphic_design_image_tablet from "../../assets/home/tablet/image-graphic-design.jpg";
-import graphic_design_image_desktop from "../../assets/home/desktop/image-graphic-design.jpg";
+import {
+  web_design_images,
+  app_design_images,
+  graphic_design_images,
+} from "../../data";
 
 const ViewProjects = () => {
-  const web_design_images = {
-    mobile: web_design_image_mobile,
-    tablet: web_design_image_tablet,
-    desktop: web_design_image_desktop,
-  };
-
-  const app_design_images = {
-    mobile: app_design_image_mobile,
-    tablet: app_design_image_tablet,
-    desktop: app_design_image_desktop,
-  };
-
-  const graphic_design_images = {
-    mobile: graphic_design_image_mobile,
-    tablet: graphic_design_image_tablet,
-    desktop: graphic_design_image_desktop,
-  };
-
   return (
     <ViewProjectsStyle>
       <ViewProject
