8bd631a7d25c4acf5ec05049c06d8ea92c4911e0
unknown <rayane.bengaoui@gmail.com>
2021-04-11 13:18:58 +0200

    Finish styling on Information component + Adding them to About page

--- src/components/About/Information.js
+++ src/components/About/Information.js
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
 
+import three_circle_bg from "../../assets/shared/desktop/bg-pattern-three-circles.svg";
+import one_circle_bg from "../../assets/about/desktop/bg-pattern-hero-about-desktop.svg";
+
 const Information = ({ images, title, description, theme, doubletext }) => {
   return (
     <InformationStyle>
@@ -9,6 +12,7 @@ const Information = ({ images, title, description, theme, doubletext }) => {
         <h2>{title}</h2>
         <p>{description}</p>
         {doubletext && <p className="secondParagraph">{doubletext} </p>}
+        {theme === "dark" && <Circle src={one_circle_bg} />}
       </TextContainer>
     </InformationStyle>
   );
@@ -32,9 +36,14 @@ const Image = styled.div`
   }
 `;
 const TextContainer = styled.div`
+  position: relative;
+  z-index: 0;
   background-color: ${(props) =>
     props.theme === "dark" ? "#E7816B" : "#FDF3F0"};
-  color: ${(props) => (props.theme === "dark" ? "white" : "black")};
+  color: ${(props) => (props.theme === "dark" ? "white" : "#E7816B")};
+  background-image: ${(props) =>
+    props.theme === "dark" ? `` : `url(${three_circle_bg})`};
+  background-repeat: no-repeat;
   min-height: 24.5rem;
   display: flex;
   flex-direction: column;
@@ -52,6 +61,14 @@ const TextContainer = styled.div`
   .secondParagraph {
     padding-top: 1rem;
   }
+`;
+
+const Circle = styled.img`
+  position: absolute;
+  width: 20rem;
+  transform: rotate(90deg) translateX(-50%);
+  right: 0;
+  z-index: -1;
 `;
 
 export default Information;
--- src/pages/About.js
+++ src/pages/About.js
@@ -41,6 +41,17 @@ const About = () => {
           "Our team is multi-disciplinary and we are not merely interested in form — content and meaning are just as important. We give great importance to craftsmanship, service, and prompt delivery. Clients have always been impressed with our high-quality outcomes that encapsulates their brand’s story and mission."
         }
       />
+      <Information
+        images={about_deal_images}
+        title={"The real deal"}
+        description={
+          "As strategic partners in our clients’ businesses, we are ready to take on any challenge as our own. Solving real problems require empathy and collaboration, and we strive to bring a fresh perspective to every opportunity. We make design and technology more accessible and give you tools to measure success."
+        }
+        theme={"light"}
+        doubletext={
+          "We are visual storytellers in appealing and captivating ways. By combining business and marketing strategies, we inspire audiences to take action and drive real results."
+        }
+      />
       <ScrollTop />
     </AboutStyle>
   );
